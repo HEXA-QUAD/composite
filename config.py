8575e33ef05e13502a2cadff4b17f02a7b3151ff
Dwyin7 <dy2482@columbia.edu>
2023-11-17 17:50:29 -0500

    Init

---
+++ config.py
@@ -0,0 +1,23 @@
+"""config path"""
+
+
+class Config(object):
+    # SQLALCHEMY_DATABASE_URI = 'mysql://root:yin732501242@localhost/test'
+    # nSQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Natalie3399!@database-1.cvlxq8ccnbut.us-east-1.rds.amazonaws.com:3306/recommendation"
+    review_server_ip = ""
+    user_server_ip = ""
+    recommendation_server_ip = ""
+
+    pass
+
+
+class Review(object):
+    pass
+
+
+class User(object):
+    pass
+
+
+class Recommendation(object):
+    pass
---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import request, jsonify
+from config import Config, User, Recommendation, Review
+
+
+
+
+#example ?? 
+# @app.route("/api/composite/get_course", methods=["GET"])
+# def get_course():
+#     # api 1
+#     # api 2
+#     pass
+
+
+
+
